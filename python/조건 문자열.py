7cf1aeae7fbdac94f05fce784bef6a0dff455810
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2023-06-03 10:24:20 +0900

    Create 조건 문자열.py

---
+++ python/조건 문자열.py
@@ -0,0 +1,13 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if(ineq==">" and eq=="=" and n>=m):
+        answer = 1
+    elif(ineq==">" and eq=="!" and n>m):
+        answer = 1
+    elif(ineq=="<" and eq=="=" and n<=m):
+        answer = 1
+    elif(ineq=="<" and eq=="!" and n<m):
+        answer = 1
+    else:
+        answer = 0
+    return answer
