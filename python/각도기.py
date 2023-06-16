43c7f4279e056e15274a6b8e2b23c3ab02f4bd1b
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2023-06-16 21:33:52 +0900

    Add files via upload

---
+++ python/각도기.py
@@ -0,0 +1,9 @@
+def solution(angle):
+    if 90> angle >0 :
+        return 1
+    elif angle == 90 :
+        return 2
+    elif 180 > angle > 90 :
+        return 3
+    elif angle == 180 :
+        return 4
