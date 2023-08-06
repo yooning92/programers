1b40b1379726c04f41103f03ab37e251f486610a
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2023-08-06 20:48:31 +0900

    Add files via upload

---
+++ python/최댓값 만들기(2).py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+         for j in range(len(numbers)):
+                if i == j:
+                    pass
+                else: 
+                    answer.append(numbers[i] * numbers[j])
+    return max(answer)
