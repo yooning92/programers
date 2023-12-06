556c8773ca246f18652d766cb8bb5a1a36c8d528
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2023-12-06 22:47:54 +0900

    Add files via upload

---
+++ java/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].contains("ad")) {
+                continue;
+            }else {
+                answer.add(strArr[i]);
+            }
+        }
+        return answer.stream().toArray(String[]::new);
+    }
+}
