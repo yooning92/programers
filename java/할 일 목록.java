acf7402536157574a847140023ed4d22c3b109ec
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2023-12-11 23:14:48 +0900

    Add files via upload

---
+++ java/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < todo_list.length; i++) {
+            if(finished[i] == false) {
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer.stream().toArray(String[]::new);
+    }
+}
