3815c614a58bdb261c64e1c4174d20dda46d2c7e
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2023-12-21 23:11:07 +0900

    Update 1로 만들기.py

--- python/1로 만들기.py
+++ python/1로 만들기.py
@@ -1,1 +1,11 @@
- 
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while(i != 1):
+            if(i % 2 == 0):
+                i /= 2
+            else:
+                i -= 1
+                i /= 2
+            answer += 1
+    return answer
