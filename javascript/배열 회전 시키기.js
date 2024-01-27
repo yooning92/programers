26a7de60ea2d9892886be4939097d43727cbdf7b
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2024-01-27 13:13:52 +0900

    Add files via upload

---
+++ javascript/배열 회전 시키기.js
@@ -0,0 +1,8 @@
+const solution = (numbers, direction) => {
+    if(direction === 'right') {
+        numbers.unshift(numbers.pop())
+    } else {
+        numbers.push(numbers.shift())
+    }
+    return numbers
+}
