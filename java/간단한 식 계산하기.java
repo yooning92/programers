95e95b3d9474e12920ecec89d8b81235bb7bc316
yoon <brv19@naver.com>
2024-03-25 21:34:45 +0900

    간단한 식 계산하기.java

---
+++ java/간단한 식 계산하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String binomial) {
+        String[] arr = binomial.split(" ");
+        String op = arr[1];
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[2]);
+        
+        if(op.equals("+")){
+            return a + b;
+        }else if(op.equals("-")){
+            return a - b;
+        }else {
+            return a * b;
+        }
+    }
+}
