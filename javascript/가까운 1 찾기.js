9194c14f92135e4829324147d34951c6a97b1d15
yoon <brv19@naver.com>
2024-04-18 21:09:13 +0900

    가까운 1 찾기.js

---
+++ javascript/가까운 1 찾기.js
@@ -0,0 +1,10 @@
+const solution = (arr, idx) => {
+    let answer = -1;
+    for(let i=idx; i < arr.length; i++) {
+        if(arr[i] == 1) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
