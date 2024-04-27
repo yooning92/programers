7dbf7a5317a71b950f697529687dd95a605a47e2
yoon <brv19@naver.com>
2024-04-27 13:57:53 +0900

    문자열 잘라서 정렬하기.java

---
+++ java/문자열 잘라서 정렬하기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String myString) {
+        return Arrays.stream(myString.split("x")).filter(s -> !s.isEmpty()).sorted().toArray(String[]::new);
+    }
+}
