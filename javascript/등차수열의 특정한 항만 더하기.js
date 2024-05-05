d07c04d211baa248cdf058fd30ba217c96912773
yoon <brv19@naver.com>
2024-05-05 12:12:14 +0900

    등차수열의 특정한 항만 더하기.js

---
+++ javascript/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,9 @@
+const solution = (a, d, included) => {
+    let answer = 0;
+    for(let i = 0; i < included.length; i++) {
+        if(included[i] == true) {
+            answer += (a + d * i);
+        }
+    }
+    return answer;
+}
