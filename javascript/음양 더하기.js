65c46659296ab73cb0431cd08a71634f9bf29332
yoon <brv19@naver.com>
2024-06-16 11:25:27 +0900

    음양 더하기.js

---
+++ javascript/음양 더하기.js
@@ -0,0 +1,11 @@
+const solution = (absolutes, signs) => {
+    let answer = 0;
+    for(let i = 0; i < signs.length; i++) {
+        if(signs[i] === true) {
+            answer += absolutes[i];
+        }else {
+            answer -= absolutes[i];            
+        }
+    }
+    return answer;
+}
