5d08ab7394124290cf7042db80a4879145e7c897
yoon <brv19@naver.com>
2024-06-24 15:36:26 +0900

    정수 제곱근 판별.java

---
+++ java/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class Solution {
+    public long solution(long n) {
+        double x = Math.sqrt(n);
+        
+        if(x % 1 == 0) {
+            return (long)Math.pow(x+1, 2);
+        }else {
+            return -1;
+        }
+    }
+}
