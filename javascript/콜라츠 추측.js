7da518d62ca4a3693c04022fb3cedcc2fb2a88f5
Jeong Seongyoon <skt.p188603@partner.sk.com>
2024-06-26 19:40:25 +0900

    콜라츠 추측.js

---
+++ javascript/콜라츠 추측.js
@@ -0,0 +1,10 @@
+const solution = (num) => {
+    let count = 0;
+    
+    while(count <= 500) {
+        if(num === 1) return count;
+        count++;
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    }
+    return -1;
+}
