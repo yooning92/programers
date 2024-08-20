4d4970071aa41859010bfd0f725c57b28db4d2a3
yooning92 <brv19@naver.com>
2024-08-20 16:58:26 +0900

    크기가 작은 부분문자열.java

---
+++ java/크기가 작은 부분문자열.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for(int i = 0; i <= t.length() - p.length(); i++) {
+            if(Long.parseLong(p) >= Long.parseLong(t.substring(i, i+p.length()))){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
