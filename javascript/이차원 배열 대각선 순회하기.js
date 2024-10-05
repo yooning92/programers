e25681c183d4ca78e3cccaebb7d06e295f396fd2
yooning92 <brv19@naver.com>
2024-10-05 11:48:03 +0900

    이차원 배열 대각선 순회하기.js

---
+++ javascript/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,11 @@
+const solution = (board, k) => {
+    let answer = 0;
+    for (let i = 0; i < board.length; i += 1) {
+        for (let j = 0; j < board[0].length; j += 1) {
+            if (i + j <= k) {
+                answer += board[i][j];
+            }
+        }
+    }
+    return answer;
+  }
