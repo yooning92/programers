48319efd87ef6b7d9eac2e6586cb64f38af42d03
yooning92 <brv19@naver.com>
2024-10-11 11:54:43 +0900

    합성수 찾기.js

---
+++ javascript/합성수 찾기.js
@@ -0,0 +1,13 @@
+const solution = (n) => {
+    let arr = [];
+
+    for(let i = 1; i <= n; i ++) {
+        for(let j = 2; j < i; j++) {
+            if(i % j === 0) {
+                arr.push(i)
+            }
+        }
+    }
+    
+    return [...new Set(arr)].length
+}
