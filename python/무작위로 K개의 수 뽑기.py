70b5604696e14228e129cbc48a6e9a9a8fc86d8d
Seong-yoon Jeong <105275248+yooning92@users.noreply.github.com>
2024-12-17 19:55:15 +0900

    무작위로 K개의 수 뽑기.py

---
+++ python/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,8 @@
+﻿def solution(arr, k):
+    answer = []
+    for i in arr:
+        if i not in answer:
+            answer.append(i)
+        if len(answer) == k:
+            break
+    return answer + [-1] * (k - len(answer))
